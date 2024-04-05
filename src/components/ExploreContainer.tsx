f4b05dd441bee6ce08adbcb19dbcb822d9c706e3
geovannysan <geovanny1santiago@gmail.com>
2024-04-05 16:56:26 -0500

    agragndo los datos del contenedor

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './ExploreContainer.css';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-import { IonBadge, IonButton, IonButtons, IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonModal, IonTitle, IonToolbar, useIonViewWillLeave } from '@ionic/react';
+import { IonBadge, IonButton, IonButtons, IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonModal, IonSegment, IonSegmentButton, IonTitle, IonToolbar, useIonViewWillLeave } from '@ionic/react';
 import { decode } from 'js-base64';
 import { Boleto } from '../utils';
 import { close, chevronForward, umbrella } from "ionicons/icons";
-
 const ExploreContainer: React.FC = () => {
   const [barcodeData, setBarcodeData] = useState('');
   const [scanActivo, setScanActivo] = useState(false);
   const [datosScn, setScan] = useState<any>({ id: "" })
   const [showModal, setModal] = useState(false);
-
+  const [tabs, setTabs] = useState('all')
   const checkPermisos = async () => {
     try {
       const status = await BarcodeScanner.checkPermission({ force: true });
@@ -29,24 +28,19 @@ const ExploreContainer: React.FC = () => {
 
   const guardarDatosScann = async (e: any) => {
     const ides: any = document.getElementById('barcode')
-    //console.log(e, ides.value)
-    //return
     if (e != "Enter") return
     if (ides.value.length > 6 && !Number.isInteger(ides.value)) {
       setScanActivo(false);
       const dat: any = JSON.parse(decode(ides.value))
       setScan({ ...datosScn, ...JSON.parse(decode(ides.value)) });
-
       if (dat.id) {
         ides.value = dat.id
         setBarcodeData(dat.id);
         const data: any = await Boleto(dat.id);
         if (!data.estado) return alert(data.mensaje);
-
         console.log(data)
         setScan({ ...datosScn, ...data.boleto });
         setModal(true)
-        // alert(JSON.stringify(data));
 
       } else {
         setBarcodeData("")
@@ -56,13 +50,11 @@ const ExploreContainer: React.FC = () => {
       setBarcodeData(ides.value);
       if (ides.value) {
         setBarcodeData(ides.value);
-
         const data: any = await Boleto(ides.value);
         if (!data.estado) return alert(data.mensaje);
         console.log(data)
         setScan({ ...datosScn, ...data.boleto });
         setModal(true)
-        //alert(JSON.stringify(data));
       } else {
         setBarcodeData("")
       }
@@ -73,7 +65,6 @@ const ExploreContainer: React.FC = () => {
     if (allow) {
       setScanActivo(true);
       (window.document.querySelector('ion-app') as HTMLElement).classList.add('cameraView');
-      //(window.document.getElementById('content') as HTMLElement).classList.add('d-none');
       BarcodeScanner.hideBackground();
       try {
         const result = await BarcodeScanner.startScan();
@@ -87,12 +78,9 @@ const ExploreContainer: React.FC = () => {
             const data: any = await Boleto(dat.id);
             setModal(true)
             setScan({ ...datosScn, ...data.boleto });
-            // alert(JSON.stringify(data));
           } else {
             setBarcodeData("")
           }
-          ///const dat:any = JSON.parse(decode(result.content))
-          // alert(JSON.stringify(dat));
           (window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
           // (window.document.getElementById('content') as HTMLElement).classList.remove('d-none');
         } else {
@@ -112,7 +100,11 @@ const ExploreContainer: React.FC = () => {
     setScanActivo(false);
     (window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
   };
-
+ function cerrarModla(){
+   setScan({})
+   setModal(false)
+ }
+  const verificarundefiner=(e:any)=> e!=undefined ? e :"valor indefinido"
   useIonViewWillLeave(() => {
     BarcodeScanner.stopScan();
     setScanActivo(false);
@@ -124,9 +116,9 @@ const ExploreContainer: React.FC = () => {
         background: "#ffffff !important"
       }}
     >
-      <IonModal isOpen={showModal}
-        onDidDismiss={() => setModal(false)}
-        initialBreakpoint={0.75} breakpoints={[0, 0.25, 0.5, 0.75, 1]}
+     {showModal? <IonModal isOpen={showModal}
+        onDidDismiss={cerrarModla}
+        initialBreakpoint={1} breakpoints={[0, 0.25, 0.5, 1, 1]}
         backdropDismiss={false}>
         <IonHeader className=" bg-welcome">
           <IonToolbar className=""
@@ -134,18 +126,18 @@ const ExploreContainer: React.FC = () => {
               color: "#ffffs"
             }}
           >
-            
+
 
             <IonButtons slot="end"  >
               <IonButton
-                onClick={() => setModal(false)}>
+                onClick={cerrarModla}>
                 <IonIcon className=" fw-bold" size="large" icon={close} ></IonIcon>
               </IonButton>
             </IonButtons>
           </IonToolbar>
         </IonHeader>
         <IonToolbar>
-          {Object.values(datosScn).length > 2 ? <div className=''>
+          {Object.values(datosScn).length > 2 ? <div className=' text-white font-serif text-4xl'>
 
             {datosScn.canjeBoleto == "NO CANJEADO" ?
               <div className='bg-success container-fluid  fw-bold text-center' style={{
@@ -156,9 +148,22 @@ const ExploreContainer: React.FC = () => {
             }
           </div> : ''}
         </IonToolbar>
+        <IonToolbar>
+          <IonSegment value={tabs} onIonChange={e => setTabs("" + e.detail.value)}>
+            <IonSegmentButton value="all">
+              <IonLabel>All</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="favorites">
+              <IonLabel>Comentario</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="Detalle">
+              <IonLabel>Detalle</IonLabel>
+            </IonSegmentButton>
+          </IonSegment>
+        </IonToolbar>
         <IonContent>
-          
-          <IonList>
+
+          {tabs == "all" ? <IonList>
             {Object.values(datosScn).length > 2 ?
 
               Object.values(datosScn).map((item, ind) => (
@@ -172,8 +177,80 @@ const ExploreContainer: React.FC = () => {
 
               )
               : ""}
-          </IonList>
-          
+          </IonList> : ""}
+          {tabs == "Detalle" ?
+            <IonContent>
+              <div className='p-2'>
+
+
+                <article className="job-card text-center ">
+                  <div>
+                    <p className="text-title">{!datosScn.info_concierto?"":JSON.parse(datosScn.info_concierto)[0].nombreConcierto || "no encontrado"}</p>
+                    <p className="post-date">Creado: {verificarundefiner(datosScn.fechaCreacion )|| "no encontrado"}</p>
+                  </div>
+
+
+                  {JSON.parse(datosScn.info_concierto)!=undefined?JSON.parse(datosScn.info_concierto).map((el: any, ind: number) => {
+                    return (<div className="budget-exp text-center" key={ind}>
+                      <div >
+                        <p className="value">${el.localidad_precio || "no encontrado"}</p>
+
+                      </div>
+                      <div>
+                        <p className="value">{el.localidad_nombre || "no encontrado"}</p>
+
+                      </div>
+                      <div>
+                        <p className="value">{el.cantidad || "no encontrado"}</p>
+
+                      </div>
+                      
+                     
+                    </div>
+                    )
+                  }):""}
+                  <div className="budget-exp" >
+                    <div >
+                      <p className="value"></p>
+                      <p className="label">Valor U.</p>
+                    </div>
+                    <div>
+                      <p className="value"></p>
+                      <p className="label">Localidad</p>
+                    </div>
+                    
+                    <div>
+                      <p className="value"></p>
+                      <p className="label">Cantidad</p>
+                    </div>
+                  </div>
+
+                  <p className="text-body">
+                    Compra realizada vía {datosScn.canal || "no encontrado"}, valor total de la compra del registro #<span className=' fw-bold'>{datosScn.id}</span> 
+                    <span className=' fw-bold'> ${datosScn.total_pago || "valor no encontrado"} </span> 
+                    cédula # {datosScn.cedula || "cedula no encontrada"} 
+                  </p>
+
+                  <div className="tags ">
+                    <article className=' fw-bold'>
+                      <p>{datosScn.forma_pago || "Forma de pago"}</p>
+                      <p>{datosScn.consolidado || "No consolidado"}</p>
+                      <p>{datosScn.estado_pago || "No pagado"}</p>
+                    </article>
+                  </div>
+
+                  <div className=''>
+                    <a >
+                      <button className="card-btn">{datosScn.estado_autorizacion_sri?"FACTURADO":"NO FACTURADO"}</button>
+                    </a>
+                  </div>
+                </article>
+              </div>
+            </IonContent>
+            : ""
+
+          }
+
         </IonContent>
         <IonFooter>
           <IonToolbar>
@@ -185,7 +262,7 @@ const ExploreContainer: React.FC = () => {
             </div> : ''}
           </IonToolbar>
         </IonFooter>
-      </IonModal >
+      </IonModal >:""}
       <div className="container">
         <strong style={{
           display: 'none'
@@ -204,9 +281,10 @@ const ExploreContainer: React.FC = () => {
               <IonInput
                 id='barcode'
                 placeholder='Código de barras o QR'
-
+                type="number"
                 onKeyPress={e => guardarDatosScann(e.key)}
                 counter={true}
+                clearInput={true}
               />
             </IonCol>
           </IonGrid>
